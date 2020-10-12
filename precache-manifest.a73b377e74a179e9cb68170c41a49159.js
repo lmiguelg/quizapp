a5fff7501afa6b2e763b66fdb3755ae38210cd98
Miguel Gonçalves <miguel.goncalves@grupoacin.local>
2020-10-12 16:57:38 +0100

    Updates

---
+++ precache-manifest.a73b377e74a179e9cb68170c41a49159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3026adf88eb569a23f182cd43f62bf6",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "6c312c90a13bddf92d51",
+    "url": "/quizapp/static/css/main.2684ee75.chunk.css"
+  },
+  {
+    "revision": "6ff6eb4cf4be82543313",
+    "url": "/quizapp/static/js/2.e54f6c0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quizapp/static/js/2.e54f6c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c312c90a13bddf92d51",
+    "url": "/quizapp/static/js/main.cb9821ac.chunk.js"
+  },
+  {
+    "revision": "a62ef0b2fb6c3b1c6c9c",
+    "url": "/quizapp/static/js/runtime-main.48fb7a81.js"
+  }
+]);
